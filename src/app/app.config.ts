c7df3c84d024d35226321a112cadfff1fea39c95
ningj <jnin044@aucklanduni.ac.nz>
2025-03-27 23:47:27 +0100

    bug fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,12 +12,12 @@ import { provideRouter } from '@angular/router';
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
+      provideFirebaseApp(() => initializeApp(environment.firebase)) as any,
+      provideFirestore(() => getFirestore()) as any,
+      provideAuth(() => getAuth()) as any,
+      provideFunctions(() => getFunctions()) as any,
+      provideStorage(() => getStorage()) as any,
+      provideMessaging(() => getMessaging()) as any
     ),
     provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-82923","appId":"1:632097657797:web:d814a1f84093b9e1e731ac","storageBucket":"friendlychat-82923.firebasestorage.app","apiKey":"AIzaSyB7df9B5U8Qt0BQ6q_0ayShOalgXzsvgKg","authDomain":"friendlychat-82923.firebaseapp.com","messagingSenderId":"632097657797"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
